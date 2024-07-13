b7e18069d3ef7e8f50ebe849978e63beafee9b3c
Pavel Khanov <pkhanov@gmail.com>
2024-07-13 22:14:17 +0300

    add getByPassport test and some exploring with `provide()`

--- README.md
+++ README.md
@@ -10,15 +10,21 @@ I'm not a professional developer. This is just an educational project. I will be
 
 1. Using latest version of ZIO stack (12.07.2024) -- http, quill
 2. build.sbt focus on things only I know. Simpler is better to start for me.
-2. More practice in ZIO DI by Service Pattern with ZLayers
-3. Create some CRUD operations on Account and Customer entities
-4. Trying to use Scala 3 opaque types for Id's (scala 3 analogue for value classes). Little confusing about implicits for codecs and etc.
-I don't know how I could optimize this. Quill does not automatically decode opaque types.(?)
+3. More practice in ZIO DI by Service Pattern with ZLayers
+4. Create some CRUD operations on Account and Customer entities
+5. ❓ Trying to use Scala 3 opaque types for Id's (scala 3 analogue for value classes). Little confusing about implicits for codecs and etc.
+I don't know how I could optimize this. Quill does not automatically decode opaque types
+6. ✅ Replace opaque types from par.5 with NewType by `"io.github.kitlangton" %% "neotype"`. Full interoperable with zio-json and zio-quill. Like it.
+7. Add first simple test. 
+    - There were some difficulties with the `provide()`. It matters where you use this `provide()`, for a `test` or a `suite`.
+    -  `TestAspect.withLiveRandom` use for live random service impl. Usefull if run test several times and have unique constraint in DB (for example). Otherwise SQL constaraint violation error occurs.
 
 ## Future
 
 - more business operations with accounts and customers
 - add test
+- testcontainers
+- programmatic migration
 - add logging
 - add errors and error handling
 - add configuration
--- src/test/scala/rohan/services/CustomerServiceSpec.scala
+++ src/test/scala/rohan/services/CustomerServiceSpec.scala
@@ -5,26 +5,35 @@ import zio.*
 import rohan.customers.*
 import io.getquill.jdbczio.Quill
 import io.getquill.SnakeCase
+import rohan.customers.CustomerService.create
 
 object CustomerServiceSpec extends ZIOSpecDefault:
   override def spec: Spec[Environment & (TestEnvironment & Scope), Any] =
-    suite("Add new customer in DB")(
+    val cc = CreateCustomer(
+      firstName = "Sara",
+      lastName = "Lara",
+      address = "Jasum pl.",
+      email = "valeron@uo.uo",
+      phone = "+85438222",
+      passport = 32341342
+    )
+    suite("CustomerService operations")(
       test("return true for added new customer") {
-        val cc = CreateCustomer(
-          firstName = "Sara",
-          lastName = "Lara",
-          address = "Jasum pl.",
-          email = "valeron@uo.uo",
-          phone = "+85438222",
-          passport = 32341342
-        )
         for
           customerId <- CustomerService.create(cc)
           inserted   <- CustomerService.getById(customerId)
-        yield assertTrue(inserted.get.firstName == "Sara")
-      }.provide(
-        CustomerServiceLive.layer,
-        Quill.Postgres.fromNamingStrategy(SnakeCase),
-        Quill.DataSource.fromPrefix("datasource")
+        yield assertTrue(inserted.get.firstName == cc.firstName)
+      },
+      test("return true if find customer by passport")(
+        for
+          cuustomerId        <- CustomerService.create(cc)
+          customerByPassport <- CustomerService.getByPassport(cc.passport)
+          _ <- Console.printLine(s"${customerByPassport.get.passport} == ${cc.passport}")
+        yield assertTrue(customerByPassport.get.passport == cc.passport)
       )
-    ) @@ TestAspect.withLiveRandom
+    ).provide(
+      CustomerServiceLive.layer,
+      Quill.Postgres.fromNamingStrategy(SnakeCase),
+      Quill.DataSource.fromPrefix("datasource")
+    )
+      @@ TestAspect.withLiveRandom
