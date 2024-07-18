0265fcccb74e6edbf3e3f3c8271d9022aeed41d9
Pavel Khanov <pkhanov@gmail.com>
2024-07-18 19:54:32 +0300

    create all routes for service ops

--- src/main/scala/rohan/accounts/AccountRoutes.scala
+++ src/main/scala/rohan/accounts/AccountRoutes.scala
@@ -2,21 +2,31 @@ package rohan.accounts
 
 import zio.http.*
 import java.util.UUID
-import rohan.types.CustomerId
+import rohan.types.*
 import zio.json.EncoderOps
 import zio.schema.codec.JsonCodec.schemaBasedBinaryCodec
 import zio.ZLayer
 import rohan.ServerUtils
+import neotype.interop.ziojson.given
 
 final case class AccountRoutes(accountService: AccountService):
 
   val all = Routes(
+    // Get all accounts
+    Method.GET / "accounts" -> handler {
+      for accounts <- accountService.getAll
+      yield Response.json(accounts.toJson)
+    },
     // Get all accounts for given customerId
     Method.GET / "customers" / uuid("customerId") / "accounts" -> handler {
       (customerId: UUID, _: Request) =>
         for acc <- accountService.getAllByCustomerId(CustomerId(customerId))
         yield Response.json(acc.toJson)
-
+    },
+    Method.GET / "accounts" / uuid("accountId") -> handler {
+      (accountId: UUID, _: Request) =>
+        for acc <- accountService.getById(AccountId(accountId))
+        yield Response.json(acc.toJson)
     },
     Method.POST / "accounts" -> handler { (req: Request) =>
       for
@@ -31,6 +41,12 @@ final case class AccountRoutes(accountService: AccountService):
           au
         ) // TODO add logs and quill exception handlers  -- .catchAll(c => zio.Console.printLine(c))
       yield Response.ok
+    },
+    Method.DELETE / "accounts" / uuid("accountId") -> handler {
+      (accountId: UUID, _: Request) =>
+        accountService
+          .delete(AccountId(accountId))
+          .map(id => Response.json(id.toJson))
     }
   ).handleError(e => Response.internalServerError(e.toString()))
 
